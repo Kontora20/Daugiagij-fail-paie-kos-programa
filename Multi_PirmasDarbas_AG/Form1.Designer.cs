d571bb2410f98ad818a16a3bbf42b2f4ccc83a97
Kontora20 <gedvilasarnas@gmail.com>
2017-06-06 17:08:58 +0300

    Daugiagijė failų paieškos programa

---
+++ Multi_PirmasDarbas_AG/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Multi_PirmasDarbas_AG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtdir2 = new System.Windows.Forms.RichTextBox();
+            this.txtFailas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDirektorija = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pasirinkti direktoriją";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(496, 135);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(446, 20);
+            this.txtInfo.TabIndex = 1;
+            // 
+            // txtdir2
+            // 
+            this.txtdir2.Location = new System.Drawing.Point(496, 33);
+            this.txtdir2.Name = "txtdir2";
+            this.txtdir2.Size = new System.Drawing.Size(446, 96);
+            this.txtdir2.TabIndex = 3;
+            this.txtdir2.Text = "";
+            // 
+            // txtFailas
+            // 
+            this.txtFailas.Location = new System.Drawing.Point(12, 96);
+            this.txtFailas.Name = "txtFailas";
+            this.txtFailas.Size = new System.Drawing.Size(196, 20);
+            this.txtFailas.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Įveskite ieškomo failo pavadinimą";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Pradėti failo paiešką";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(134, 49);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(216, 20);
+            this.txtPath.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Pasirinkta direktorija";
+            // 
+            // txtDirektorija
+            // 
+            this.txtDirektorija.Location = new System.Drawing.Point(279, 96);
+            this.txtDirektorija.Name = "txtDirektorija";
+            this.txtDirektorija.Size = new System.Drawing.Size(189, 20);
+            this.txtDirektorija.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Įveskite ieškomos direktorijos pavadinimą";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(279, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Pradėti direktorijos paiešką";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(212, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Thread Sleep";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(496, 161);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(446, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(496, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Paieška užtruko:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(589, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = ".";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(707, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 367);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDirektorija);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFailas);
+            this.Controls.Add(this.txtdir2);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.RichTextBox txtdir2;
+        private System.Windows.Forms.TextBox txtFailas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDirektorija;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Multi_PirmasDarbas_AG/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Multi_PirmasDarbas_AG
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        private string kelias;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                kelias = fbd.SelectedPath;
+                txtPath.Text = kelias;
+            }
+            else
+            {
+                MessageBox.Show("Neteisingas pasirinkimas");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Thread thread = new Thread(new ParameterizedThreadStart(PaieskaFailo));
+
+            if (txtFailas.Text != "" && kelias != "")
+            {
+                thread.Start(kelias);
+            }
+            else
+            {
+                MessageBox.Show("Pirma įveskite ieškomo failo pavadinimą");
+            }
+        }
+
+        private void PaieskaFailo(object kelias)
+        {
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Invoke(new EventHandler(delegate { txtdir2.Clear(); }));
+            Invoke(new EventHandler(delegate { txtInfo.Clear(); }));
+
+            string ieskomas = txtFailas.Text;
+
+            string[] dirs = Directory.GetFiles((string)kelias, ieskomas + "*.*", SearchOption.AllDirectories);
+
+            int current = 0;
+
+
+            foreach (string dir in dirs)
+            {
+                try
+                {
+                    current++;
+                    Invoke(new EventHandler(delegate { txtdir2.Text += "Failas: " + new DirectoryInfo(dir).Name + "\n"; }));
+                    Invoke(new EventHandler(delegate { txtdir2.Text += "Failo direktorija: " + dir + "\n"; }));
+                    Invoke(new EventHandler(delegate { txtdir2.Text += "\n"; }));
+                    Invoke(new EventHandler(delegate { progressBar1.Value = (100 * current / dirs.Length); }));
+
+                    Invoke(new EventHandler(delegate { label6.Text = progressBar1.Value.ToString() + "%"; }));
+                    Thread.Sleep(100);
+
+                    if (dirs.Length <= 0)
+                    {
+                        Invoke(new EventHandler(delegate { txtInfo.Text = "Nerasta tokių failų"; }));
+                    }
+                    else
+                    {
+                        Invoke(new EventHandler(delegate { txtInfo.Text = "Direktorijų kiekis, kuriose rastas failas: " + dirs.Length; }));
+                    }
+                }
+
+                catch (UnauthorizedAccessException e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+
+            }
+
+            sw.Stop();
+            BeginInvoke((MethodInvoker)delegate { label5.Text = sw.Elapsed.Milliseconds + "ms"; });
+
+            //---------------------------------------REKURSIJA---------------------------------------------------------------------------------
+
+            //Stopwatch sw = Stopwatch.StartNew();
+
+            //string ieskomas = txtFailas.Text;
+            //int current = 0;
+            //string[] dirs = Directory.GetDirectories((string)kelias);
+            //int counter;
+
+            //foreach (string dir in dirs)
+            //{
+
+            //    try
+            //    {
+            //        foreach (string file in Directory.GetFiles(dir, ieskomas + "*.*", SearchOption.AllDirectories))
+            //        {
+            //            Invoke(new EventHandler(delegate { txtdir2.Text += "Failas: " + new DirectoryInfo(file).Name + "\n"; }));
+            //            Invoke(new EventHandler(delegate { txtdir2.Text += "Failo direktorija: " + dir + "\n"; }));
+            //            Invoke(new EventHandler(delegate { txtdir2.Text += "\n"; }));
+
+            //        }
+            //    }
+            //    catch (UnauthorizedAccessException)
+            //    {
+            //        continue;
+            //    }
+
+            //    //Thread.Sleep(200);
+
+            //    PaieskaFailo(dir);
+            //    // counter = +Convert.ToInt32(dirs.Length);
+            //    current++;
+            //    Invoke(new EventHandler(delegate { progressBar1.Value = (100 * current / dirs.Length); }));
+            //    Invoke(new EventHandler(delegate { label6.Text = progressBar1.Value.ToString() + "%"; }));
+            //}
+
+            //Invoke(new EventHandler(delegate { txtInfo.Text = "Direktorijų kiekis, kuriose rastas failas: " + dirs.Length; }));
+
+            //sw.Stop();
+            //Invoke(new EventHandler(delegate { label5.Text = sw.Elapsed.Milliseconds + "ms"; }));
+
+        }
+
+
+
+        private void PaieskaDirektorijos(object kelias)
+        {
+            try
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+
+                Invoke(new EventHandler(delegate { txtdir2.Clear(); }));
+                Invoke(new EventHandler(delegate { txtInfo.Clear(); }));
+
+                string ieskomas = txtDirektorija.Text;
+
+                string[] dirs = Directory.GetFiles((string)kelias, ieskomas + "*.*", SearchOption.AllDirectories);
+
+                int current = 0;
+
+                foreach (string dir in dirs)
+                {
+
+                    try
+                    {
+                        current++;
+                        //Invoke(new EventHandler(delegate { txtdir2.Text += "Failas: " + new DirectoryInfo(dir).Name + "\n"; }));
+                        Invoke(new EventHandler(delegate { txtdir2.Text += "Failo direktorija: " + dir + "\n"; }));
+                        Invoke(new EventHandler(delegate { txtdir2.Text += "\n"; }));
+                        Invoke(new EventHandler(delegate { progressBar1.Value = (100 * current / dirs.Length); }));
+
+                        Invoke(new EventHandler(delegate { label6.Text = progressBar1.Value.ToString() + "%"; }));
+                        //Thread.Sleep(100);
+
+                        if (dirs.Length == 0)
+                        {
+                            Invoke(new EventHandler(delegate { txtInfo.Text = "Nerasta tokių direktorijų"; }));
+                        }
+                        else
+                        {
+                            Invoke(new EventHandler(delegate { txtInfo.Text = "Rastas direktorijų kiekis: " + dirs.Length; }));
+                        }
+                    }
+
+                    catch (UnauthorizedAccessException)
+                    {
+                        //continue;
+                    }
+
+                }
+
+                sw.Stop();
+                BeginInvoke((MethodInvoker)delegate { label5.Text = sw.Elapsed.Milliseconds + "ms"; });
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(PaieskaDirektorijos));
+
+            if (txtDirektorija.Text != "" && kelias != "")
+            {
+
+                thread.Start(kelias);
+            }
+            else
+            {
+                MessageBox.Show("Pirma įveskite ieškomo failo pavadinimą");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
